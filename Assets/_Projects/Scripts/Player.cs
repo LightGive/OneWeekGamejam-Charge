4f45a043123a6f9c2e450a4064311abde38ff69a
LightGive <roi.akase@outlook.jp>
2022-09-06 01:00:05 +0900

    敵に当たったときに点滅、無敵処理

--- Assets/_Projects/Scripts/Player.cs
+++ Assets/_Projects/Scripts/Player.cs
@@ -8,12 +8,20 @@ namespace OneWeekGamejam.Charge
 	public class Player : MonoBehaviour
 	{
 		[SerializeField, Range(0.01f, 10.0f)] float _maxTime = 3.0f;
+		const string AnimParamDamage = "Damage";
+		const string AnimParamNormal = "Normal";
+		const int MaxHitpoint = 3;
+		const int StartChageLevel = 2;
+		const int MaxChageLevel = 4;
+		[SerializeField] Animation _anim = null;
 		[SerializeField] float _moveSpeed = 1.0f;
 		[SerializeField] float _stickAngleThresholdStart = 0.5f;
 		[SerializeField] float _stickAngleThresholdEnd = 0.2f;
+		bool _isInvisible = false;
 		bool _isCharge = false;
 		float _charge = 0.0f;
 		float _chargeMax = 1.0f;
+		float _invisibleTimeCnt = 0.0f;
 		Vector3 _vec = Vector3.zero;
 		Vector2 _screenCenter = Vector2.zero;
 
@@ -30,8 +38,13 @@ namespace OneWeekGamejam.Charge
 			{
 				_charge += Time.deltaTime;
 			}
+			CheckInvisible();
 		}
 
+		void OnTriggerEnter2D(Collider2D col)
+		{
+			Damage();
+		}
 
 		public void OnMove(InputAction.CallbackContext context)
 		{
@@ -80,9 +93,35 @@ namespace OneWeekGamejam.Charge
 				}
 			}
 		}
+
+		/// <summary>
+		/// �G��G�̍U������������
+		/// </summary>
+		void Damage()
+		{
+			_hp--;
+			_anim.Play(AnimParamDamage);
+			_isInvisible = true;
+		}
+
+		/// <summary>
+		/// ���G����`�F�b�N
+		/// </summary>
+		void CheckInvisible()
+		{
+			if (!_isInvisible) { return; }
+			_invisibleTimeCnt += Time.deltaTime;
+			if (_invisibleTimeCnt > _invisibleTime)
+			{
+				_invisibleTimeCnt = 0.0f;
+				_isInvisible = false;
+				_anim.Play(AnimParamNormal);
+			}
+		}
+
 		void Look(Vector2 vec)
 		{
-			var angle = Mathf.Atan2(vec.y, vec.x) * Mathf.Rad2Deg + 90.0f;
+			var angle = Mathf.Atan2(vec.y, vec.x) * Mathf.Rad2Deg - 90.0f;
 			transform.localRotation = Quaternion.Euler(0.0f, 0.0f, angle);
 		}
 
