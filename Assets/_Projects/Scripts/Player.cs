9ba26aae46214752c341637072774b432b41e39c
LightGive <roi.akase@outlook.jp>
2022-09-05 22:42:53 +0900

    入力回りを作成

--- Assets/_Projects/Scripts/Player.cs
+++ Assets/_Projects/Scripts/Player.cs
@@ -9,14 +9,27 @@ namespace OneWeekGamejam.Charge
 	{
 		[SerializeField, Range(0.01f, 10.0f)] float _maxTime = 3.0f;
 		[SerializeField] float _moveSpeed = 1.0f;
-
+		[SerializeField] float _stickAngleThresholdStart = 0.5f;
+		[SerializeField] float _stickAngleThresholdEnd = 0.2f;
+		bool _isCharge = false;
 		float _charge = 0.0f;
 		float _chargeMax = 1.0f;
 		Vector3 _vec = Vector3.zero;
+		Vector2 _screenCenter = Vector2.zero;
 
+		private void Awake()
+		{
+			_screenCenter = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
+		}
 		private void Update()
 		{
 			transform.position += _vec * _moveSpeed * Time.deltaTime;
+
+
+			if (_isCharge)
+			{
+				_charge += Time.deltaTime;
+			}
 		}
 
 
@@ -24,6 +37,63 @@ namespace OneWeekGamejam.Charge
 		{
 			_vec = context.ReadValue<Vector2>();
 			
+		}
+
+		public void OnLook(InputAction.CallbackContext context)
+		{
+			var stickVec = context.ReadValue<Vector2>();
+			var stickMag = stickVec.magnitude;
+			if (_isCharge && stickMag <= _stickAngleThresholdEnd)
+			{
+				ReleaseCharge();
+			}
+			else if(!_isCharge && stickVec.magnitude >= _stickAngleThresholdStart)
+			{
+				StartCharge();
+			}
+
+			if (_isCharge)
+			{
+				Look(stickVec);
+			}
+		}
+
+		public void OnAim(InputAction.CallbackContext context)
+		{
+			if(!_isCharge) { return; }
+			var screenPoint = context.ReadValue<Vector2>();
+			var vec = screenPoint - _screenCenter;
+			Look(vec);
+		}
+
+		public void OnMouse(InputAction.CallbackContext context)
+		{
+			if (context.performed)
+			{
+				if (context.action.IsPressed())
+				{
+					StartCharge();
+				}
+				else
+				{
+					ReleaseCharge();
+				}
+			}
+		}
+		void Look(Vector2 vec)
+		{
+			var angle = Mathf.Atan2(vec.y, vec.x) * Mathf.Rad2Deg + 90.0f;
+			transform.localRotation = Quaternion.Euler(0.0f, 0.0f, angle);
+		}
+
+		void StartCharge()
+		{
+			_isCharge = true;
+		}
+
+		void ReleaseCharge()
+		{
+			_isCharge = false;
 		}
 	}
 }
