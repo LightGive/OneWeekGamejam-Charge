11a0319407962003da894a0713239439954e3381
LightGive <roi.akase@outlook.jp>
2022-09-06 23:56:18 +0900

    URP、コミット漏れ

--- Assets/_Projects/Scripts/SceneMain.cs
+++ Assets/_Projects/Scripts/SceneMain.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LightGive.UIUtil;
 
 namespace OneWeekGamejam.Charge
 {
     public class SceneMain : MonoBehaviour
     {
+		[SerializeField] UIMain _UIMain = null;
+
 		void Start()
 		{
-			
+			_UIMain.Show();
 		}
 	}
 }
