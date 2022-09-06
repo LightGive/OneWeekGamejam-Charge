fae8070491b6b54e236710c4005ecbcf294b7db1
LightGive <roi.akase@outlook.jp>
2022-09-06 23:02:17 +0900

    Tagの名前を定数に

---
+++ Assets/LightGive/NameDatas/SceneName.cs
@@ -0,0 +1,25 @@
+public class SceneName
+
+{
+
+
+
+	/// <summary>
+
+	/// return "Main"
+
+ 	/// </summary>
+
+	public const string @Main = "Main";
+
+
+	/// <summary>
+
+	/// <para>0. "Main"</para>
+
+	/// </summary>
+
+	public static readonly string[] names = new string[]{"Main"};
+
+}
+
